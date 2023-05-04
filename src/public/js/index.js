1434e66a88425696432a4875dba78f7677e3a33f
Noelia Lavilla <noelia.m.lavilla@hotmail.com>
2023-05-04 01:03:25 -0300

    feat: parte 2

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,1 +1,16 @@
 const socket = io();
+
+let user;
+const chatbox = document.getElementById('chatBox');
+Swal.fire({
+    title: 'Identificate',
+    input: 'text',
+    text: 'Ingresa tu nombre de usuario: ',
+    inputValidator: (value) => {
+        return !value && "El nombre de usuario es obligatorio."
+    },
+    allowOutsideClick: false,
+    allowEscapeKey: false
+}).then(result =>{
+    user = result.value;
+});
